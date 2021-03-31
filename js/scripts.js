84b56edfef9c6d1eae3a2b65816aa28daa0fbb1e
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-31 16:39:26 -0700

    WIP: building functionality for any number of consonants at the beginning of a word
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,17 @@ function findConsonant2(word) {
   return false;
 }
 
+function findVowel(word) {
+  let firstLetter = word.charAt(0);
+  let vowel = ["a","e","i","o","u"]
+  for (i = 0; i < vowel.length; i++) {
+    if (firstLetter === vowel[i]) {
+      return true;
+    };
+  };
+  return false;
+};
+
 function pigLatin(word) {
   let firstLetter = word.charAt(0);
   let secondLetter = word.charAt(1);
@@ -28,13 +39,50 @@ function pigLatin(word) {
   if ((firstLetter==='a')||(firstLetter==='e')||(firstLetter==='i')||(firstLetter==='o')||(firstLetter==='u')) {
     pigLatinStr = word.concat("-way");
     return pigLatinStr;
-  } else if ((findConsonant1(word) === true) && (findConsonant2(word) === false)) {
-  pigLatinStr = word.slice(1, word.length) + "-" + firstLetter + "ay";
+    // if ((findConsonant1(word) === true) && (findConsonant2(word) === false))
+  } else  {
+  pigLatinStr = word.slice(numConsonants, word.length) + "-" + firstLetter + "ay";
 
-  } else if ((findConsonant1(word) === true) && (findConsonant2(word) === true)) {
-    pigLatinStr = word.slice(2, word.length) + "-" + firstLetter + secondLetter + "ay";
-  };
+  } 
+  //  else if ((findConsonant1(word) === true) && (findConsonant2(word) === true)) {
+  //   pigLatinStr = word.slice(2, word.length) + "-" + firstLetter + secondLetter + "ay";
+  // };
   return pigLatinStr;
 }
 
 // Mike Krzyzewski --> ike-May ewski-Krzyzay
+
+function findNumConsonants(word) {
+  let consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
+  let numConsonants = 0;
+  let firstLetter = word.charAt(0);
+  if (findVowel(firstLetter) === true) {
+    return numConsonants;
+  } else {
+    for (i = 0; i < word.length; i++) {
+      console.log("outerloop " + i + " word " + word);
+      currentLetter = word.charAt(i);
+      // if (findVowel(currentLetter) === true) {
+      //   return numConsonants;
+      // } 
+      // first loop letter s
+      for (n = 0; n < consonant.length; n++) {
+        console.log("innerloop "+ n +" currentLetter " + currentLetter);
+        // first loop letter b
+        // Check for a vowel here
+        if (currentLetter === consonant[n]) {
+          numConsonants++; 
+        }
+      };
+    }
+  }
+}
+
+
+// loop through each letter.  Break when letter not consonant.
+
+// Outer loop: iterate through each letter
+//store: First loop 's' Second loop 't' Thired loop 'o' break
+
+// Letter s: Inner loop iterate the consonate array
+
